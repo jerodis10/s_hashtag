76ba2e643e5a19b1413fab90df438b621b040bc4
jerodis10 <jerodis10@naver.com>
2022-04-08 07:46:41 +0900

    Feat: searchBox 내 keyword 검색 기능 추가 #12

--- s_hashtag-web/src/main/resources/static/js/sideBar.js
+++ s_hashtag-web/src/main/resources/static/js/sideBar.js
@@ -46,6 +46,68 @@ document.getElementById("searchBox").addEventListener('keypress', function(e) {
     }
 });
 
+document.getElementById("btn_check1").addEventListener('click', function(e){
+    e.target.classList.toggle('bi-check-lg');
+    getHashtagByCount();
+});
+
+document.getElementById("btn_check2").addEventListener('click', function(e){
+    e.target.classList.toggle('bi-check-lg');
+});
+
+document.getElementById("btn_check3").addEventListener('click', function(e){
+    e.target.classList.toggle('bi-check-lg');
+});
+
+document.getElementById("btn_check4").addEventListener('click', function(e){
+    e.target.classList.toggle('bi-check-lg');
+});
+
+document.getElementById("btn_check5").addEventListener('click', function(e){
+    e.target.classList.toggle('bi-check-lg');
+});
+
+
+function getHashtagByCount() {
+
+    var category_arr = [];
+    if(document.getElementById("btn_cafe").className === 'selected') category_arr.push(document.getElementById("btn_cafe").value);
+    if(document.getElementById("btn_food").className === 'selected') category_arr.push(document.getElementById("btn_food").value);
+    var category_param = category_arr.join(',')
+
+    $.ajax({
+       url:'/getHashtagByCount',
+       type:'GET',
+       dataType: 'json',
+       data: {ha: 126.96190764549995, oa: 126.99019733287525, pa: 37.56300112995975, qa: 37.5696007924915, category_list: category_param, searchText: searchText}, // 서울시청 주변
+       success:function(data){
+            console.log(data);
+
+            if(data){
+                $.each(data, function(d_index, d_item){
+                    $.each(marker_object['CE7'], function(index, item){
+                        if(d_item.longitude.toFixed(13) !== item.getPosition().La.toFixed(13) || d_item.latitude.toFixed(13) !== item.getPosition().Ma.toFixed(13))
+                            item.setMap(null);
+                    });
+                    $.each(overlay_object['CE7'], function(index, item){
+                        if(d_item.longitude.toFixed(13) !== item.getPosition().La.toFixed(13) || d_item.latitude.toFixed(13) !== String(item.getPosition().Ma.toFixed(13)-0.001))
+                            item.setMap(null);
+                    });
+                    $.each(marker_object['FD6'], function(index, item){
+                        if(d_item.longitude.toFixed(13) !== item.getPosition().La.toFixed(13) || d_item.latitude.toFixed(13) !== item.getPosition().Ma.toFixed(13))
+                            item.setMap(null);
+                    });
+                    $.each(overlay_object['FD6'], function(index, item){
+                        if(d_item.longitude.toFixed(13) !== item.getPosition().La.toFixed(13) || d_item.latitude.toFixed(13) !== String(item.getPosition().Ma.toFixed(13)-0.001))
+                            item.setMap(null);
+                    });
+                });
+            }
+       },
+       error : function(e){
+       }
+    });
+}
 
 
 /* EXPANDER MENU */
