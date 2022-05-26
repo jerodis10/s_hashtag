82a399b4d19967d7b2b5c61cb63bfaa79a78f912
jerodis10 <jerodis10@naver.com>
2022-05-27 07:41:23 +0900

    Feat: spring security + jwt를 통한 인증 구현 #8 (로그인)

--- s_hashtag-web/src/main/java/com/s_hashtag/auth/ApplicationUserService.java
+++ s_hashtag-web/src/main/java/com/s_hashtag/auth/ApplicationUserService.java
@@ -1,28 +1,28 @@
-//package com.s_hashtag.auth;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-////@RequiredArgsConstructor
-//public class ApplicationUserService implements UserDetailsService {
-//
-//    private final ApplicationUserDao applicationUserDao;
-//
-//    @Autowired
-//    public ApplicationUserService(@Qualifier("fake") ApplicationUserDao applicationUserDao) {
-//        this.applicationUserDao = applicationUserDao;
-//    }
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        return applicationUserDao.selectApplicationUserByUsername(username)
-//                .orElseThrow(() ->
-//                        new UsernameNotFoundException(String.format("Username %s not found", username)));
-//    }
-//}
+package com.s_hashtag.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+//@RequiredArgsConstructor
+public class ApplicationUserService implements UserDetailsService {
+
+    private final ApplicationUserDao applicationUserDao;
+
+    @Autowired
+    public ApplicationUserService(@Qualifier("fake") ApplicationUserDao applicationUserDao) {
+        this.applicationUserDao = applicationUserDao;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return applicationUserDao.selectApplicationUserByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(String.format("Username %s not found", username)));
+    }
+}
--- s_hashtag-web/src/main/java/com/s_hashtag/jwt/JwtSecretKey.java
+++ s_hashtag-web/src/main/java/com/s_hashtag/jwt/JwtSecretKey.java
@@ -1,22 +1,22 @@
-//package com.s_hashtag.jwt;
-//
-//import io.jsonwebtoken.security.Keys;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import javax.crypto.SecretKey;
-//
-//@Configuration
-//@RequiredArgsConstructor
-//public class JwtSecretKey {
-//
-//    private final JwtConfig jwtConfig;
-//
-//    @Bean
-//    public SecretKey secretKey() {
-//        return Keys.hmacShaKeyFor(jwtConfig.getSecretKey().getBytes());
-//    }
-//
-//}
+package com.s_hashtag.jwt;
+
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.SecretKey;
+
+@Configuration
+@RequiredArgsConstructor
+public class JwtSecretKey {
+
+    private final JwtConfig jwtConfig;
+
+    @Bean
+    public SecretKey secretKey() {
+        return Keys.hmacShaKeyFor(jwtConfig.getSecretKey().getBytes());
+    }
+
+}
