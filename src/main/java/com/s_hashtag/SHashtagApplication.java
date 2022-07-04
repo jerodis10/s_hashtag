95a94bab44a7a46ad2f262e71618e0b9d03467f4
jerodis10 <jerodis10@naver.com>
2022-07-05 07:45:26 +0900

    Feat: 멀티모듈 세팅 #1

---
+++ src/main/java/com/s_hashtag/SHashtagApplication.java
@@ -0,0 +1,15 @@
+package com.s_hashtag;
+
+//import com.s_hashtag.kakaoapi.domain.caller.AbProperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SHashtagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SHashtagApplication.class, args);
+	}
+
+}
