c85072f0afe47568f4fc3ef448ba44c2eb130987
jerodis10 <jerodis10@naver.com>
2022-09-06 21:56:19 +0900

    Feat: 해시태그 포스트 모달창 - 이미지 슬라이드 #1

--- s_hashtag-web/src/main/java/com/s_hashtag/api/HomeController.java
+++ s_hashtag-web/src/main/java/com/s_hashtag/api/HomeController.java
@@ -1,15 +1,23 @@
 package com.s_hashtag.api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Slf4j
+import java.net.http.HttpRequest;
+
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String homeLogin() {
         return "home";
+    }
+
+//    @CrossOrigin
+    @GetMapping("/instagramPost")
+    public String instagramPost() {
+        return "modal/instagramPost";
     }
 
 }
